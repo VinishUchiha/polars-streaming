4da5a40d3fb4899f73b165e603b6cb712b0077ba
Vinish M <vinishuchiha@gmail.com>
2023-04-29 09:26:58 +0530

    elasticsearch exception added

--- polars_streaming/es_writer.py
+++ polars_streaming/es_writer.py
@@ -1,8 +1,11 @@
-from elasticsearch import Elasticsearch
-from elasticsearch.helpers import bulk
+from .exceptions import ModuleNotFoundException
 
 class ElasticSearch():
     def __init__(self, host: str, username: str =None, password: str =None, api_key: str = None) -> None:
+        try:
+            from elasticsearch import Elasticsearch
+        except ImportError:
+            raise ModuleNotFoundException("elasticsearch not installed. try `pip install elasticsearch`.")
         if username is not None and password is not None:
             self.es = ElasticSearch([host], basic_auth=(username, password))
         elif api_key is not None:
@@ -11,6 +14,7 @@ class ElasticSearch():
             self.es = ElasticSearch([host])
 
     def write_dataframe(self, df, index):
+        from elasticsearch.helpers import bulk
         records = df.to_dict()
         actions = [
             {
