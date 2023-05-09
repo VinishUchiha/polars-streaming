0514e5804f17fbb5b15a67bd5860190f34926246
Vinish M <vinishuchiha@gmail.com>
2023-05-09 13:42:05 +0530

    es writer bug fixed

--- polars_streaming/sinks/esWriter.py
+++ polars_streaming/sinks/esWriter.py
@@ -1,20 +1,20 @@
 from ..exceptions import ModuleNotFoundException
 
-class ElasticSearch():
+class ESWriter():
     def __init__(self, host: str, index: str, username: str =None, password: str =None, api_key: str = None) -> None:
         try:
             from elasticsearch import Elasticsearch
         except ImportError:
             raise ModuleNotFoundException("elasticsearch not installed. try `pip install elasticsearch`.")
         if username is not None and password is not None:
-            self.es = ElasticSearch([host], basic_auth=(username, password))
+            self.es = Elasticsearch([host], basic_auth=(username, password))
         elif api_key is not None:
-            self.es = ElasticSearch([host], api_key=api_key)
+            self.es = Elasticsearch([host], api_key=api_key)
         else:
-            self.es = ElasticSearch([host])
+            self.es = Elasticsearch([host])
         self.index = index
 
-    def write_dataframe(self, df, index):
+    def write_dataframe(self, df):
         from elasticsearch.helpers import bulk
         records = df.to_dict()
         actions = [
